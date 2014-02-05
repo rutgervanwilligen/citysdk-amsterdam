1860de73f4eb107cf4bb88342b0852ec9ecf98fa
Bert Spaan <bert.spaan@gmail.com>
2014-02-05 11:42:37 +0100

    NWD data to NWB nodes - initial commit

---
+++ importers/ndw/README.md
@@ -0,0 +1,10 @@
+# NDW importer
+
+Adds data to existing NWB nodes in CitySDK on `ndw` layer, to be used by `ndw` webservice. This webservice uses real-time NDW data to link to NDW road segments in CitySDK upon request.
+
+Importer expects NWB road segments to be already imported - see `nwb` importer.
+Importer expects existing CitySDK layer `ndw`.
+
+See `citysdk-ndw` repository for information about obtaining data, creating NDW database, and details about linking NDW DATEX II real-time traffic flow data to NWB road segments.
+
+
---
+++ importers/ndw/importer.rb
@@ -0,0 +1,50 @@
+# NDW importer
+
+require 'sequel'
+require 'json'
+require 'citysdk'
+
+credentials = '/var/www/citysdk/shared/config/cdkpw.json'
+pw = File.exists?(credentials) ? JSON.parse(File.read(credentials)) : nil
+
+email = ARGV[0] || (pw ? pw['email'] : nil) || 'citysdk@waag.org'
+passw = ARGV[1] || (pw ? pw[email]  : nil) || ''
+host  = ARGV[2] || (pw ? pw['host']  : nil) || 'api.dev'
+
+layer = "ndw"
+
+api = CitySDK::API.new("api.citysdk.waag.org")
+api.authenticate(email, passw)
+api.set_layer layer
+
+DB = Sequel.connect("postgres://postgres:postgres@localhost/citysdk-ndw")
+
+query = <<SQL
+  SELECT
+    wvk_id
+  FROM 
+    wegvakken;
+SQL
+
+i = 0
+begin
+  DB[query].each do |row|
+    # Convert BigDecimal to int
+    wvk_id = row[:wvk_id].to_i
+    
+    node = {
+      cdk_id: "nwb.#{wvk_id}",
+      data: {
+        wvk_id: wvk_id
+      }
+    }
+    api.create_node node
+    
+    i += 1
+    puts "Imported #{i} rows" if i % 1000 == 0     
+  end
+ensure
+	api.release
+end
+
+puts "done..."
