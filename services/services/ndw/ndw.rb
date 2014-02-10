8676f502e7a46baff20bfaeee493cf935f65ff88
Bert Spaan <bert.spaan@gmail.com>
2014-02-10 17:19:18 +0100

    service stub

--- services/services/ndw/ndw.rb
+++ services/services/ndw/ndw.rb
@@ -5,7 +5,15 @@
 class CitySDK_Services < Sinatra::Base
 
   post '/ndw' do    
-    
+    json = self.parse_request_json
+        
+    return { 
+      :status => 'success', 
+      :url => request.url, 
+      :data => {
+        test: true
+      }
+    }.to_json 
   end
     
 end
