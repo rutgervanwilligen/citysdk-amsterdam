14477667258d36ca026781190a24b3c81b5c0248
Bert Spaan <bert.spaan@gmail.com>
2014-02-13 11:34:16 +0100

    finished parking capacity service

--- services/services/parking/parking.rb
+++ services/services/parking/parking.rb
@@ -8,38 +8,37 @@ class CitySDK_Services < Sinatra::Base
   PARKING_PATH = "/trafficlinkdata/wegdata/IDPA_ParkingLocation.GeoJSON"
   PARKING_TIMEOUT = 5 * 60
   
-  PARKING_MAPPING = JSON.parse(File.open(File.dirname(__FILE__) + '/mapping.json','r').read)
+  #PARKING_MAPPING = JSON.parse(File.open(File.dirname(__FILE__) + '/mapping.json','r').read)
   
-  # curl --data '{"postcode":"1103DS"}' http://localhost:9292/parking  
+  # curl --data '{"id":"CE-P01 Sloterdijk"}' http://localhost:9292/parking  
   post '/parking' do
   
     # Read data from request 
     json = self.parse_request_json
-    postcode = json["postcode"]
+    id = json["id"]
     
     # TODO: naming convention!
-    key = "parking!!!#{postcode}"
+    key = "parking!!!#{id}"
     data = CitySDK_Services.memcache_get(key)
     if not data
     
       connection = Faraday.new PARKING_HOST    
       response = httpget(connection, PARKING_PATH)
       if response.status == 200
-        # Current version of parking JSON is invalid:
-        # "key"="value" instead of "key": "value"
-        garages = JSON.parse response.body.gsub(/\"=\"/, '": "')
+        garages = JSON.parse response.body
                 
         garages["features"].each do |garage|
-          name = garage["properties"]["Name"]
+          id = garage["Id"]          
+          garage_key = "parking!!!#{id}"
           
-          data = {}
-          if PARKING_MAPPING.has_key? name
-            garage_postcode = PARKING_MAPPING[name]
-            garage_key = "parking!!!#{garage_postcode}"
+          # Convert number in properties hash to integers
+          garage["properties"]["FreeSpaceShort"] = garage["properties"]["FreeSpaceShort"].to_i
+          garage["properties"]["FreeSpaceLong"] = garage["properties"]["FreeSpaceLong"].to_i
+          garage["properties"]["ShortCapacity"] = garage["properties"]["ShortCapacity"].to_i
+          garage["properties"]["LongCapacity"] = garage["properties"]["LongCapacity"].to_i
             
-            # TODO: get timeout from layer data
-            CitySDK_Services.memcache_set(garage_key, garage["properties"], PARKING_TIMEOUT)
-          end          
+          # TODO: get timeout from layer data
+          CitySDK_Services.memcache_set(garage_key, garage["properties"], PARKING_TIMEOUT)       
         end
       end
       
@@ -51,14 +50,12 @@ class CitySDK_Services < Sinatra::Base
         data = {}
       end      
     
-    end
-    
-    json["capacity"] = data
+    end    
     
     return { 
       :status => 'success', 
       :url => request.url, 
-      :data => json
+      :data => data
     }.to_json 
 
   end
